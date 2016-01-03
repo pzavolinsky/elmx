14459b512f96f8cd99d84169b936b37c108a6273
Patricio Zavolinsky <pzavolinsky@yahoo.com.ar>
2016-01-03 01:05:54 +0000

    Updated example.

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,15 +2,17 @@
 
 var gulp = require('gulp');
 var insert = require('gulp-insert');
+var rename = require('gulp-rename');
 var elmx = require('../dist/parser');
 
-var src = './elmx/**/*.elm'
+var src = './src/**/*.elmx'
 
 gulp.task('default', ['watch']);
 
 gulp.task('build', function(cb) {
   return gulp.src(src)
     .pipe(insert.transform(elmx))
+    .pipe(rename({extname: '.elm'}))
     .pipe(gulp.dest('./dist'));
 });
 
