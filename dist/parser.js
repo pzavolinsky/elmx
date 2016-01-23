ad481c8bbf4c09484671314ab65ab4aa3cb9da21
Patricio Zavolinsky <pzavolinsky@yahoo.com.ar>
2016-01-23 02:31:26 +0000

    Removed obsolete code.

--- dist/parser.js
+++ dist/parser.js
@@ -4,25 +4,17 @@ var htmlparser = require("htmlparser2");
 var State = require("./state");
 var attrParser = require("./attributes");
 var expr = require("./expression");
-var strip = require("./strip-elmx");
 var generate = require("./generator");
 var R = require("ramda");
 
-function enableModules(content) {
-  return content.replace(/--(import Html)/g, "$1");
-}
-
-function parse(elmx) {
+module.exports = function (elmx) {
   var state = new State();
-  // let padding = "";
 
   var parser = new htmlparser.Parser({
     onopentag: function onopentag(name, attrs) {
       state.enter(name, attrParser(attrs));
     },
     ontext: function ontext(text) {
-      // padding = R.last(text.match(/(?:^|[\r\n])([ \t]*)$/) || [""]);
-
       if (state.isRoot()) {
         state.addCode(text);
         return;
@@ -43,9 +35,5 @@ function parse(elmx) {
   parser.write(elmx);
   parser.end();
 
-  // console.log('ast', state.dump());
-
   return generate(state.get());
-}
-
-module.exports = R.compose(parse, strip, enableModules);
+};
--- src/parser.js
+++ src/parser.js
@@ -2,25 +2,17 @@ const htmlparser = require("htmlparser2");
 const State = require("./state");
 const attrParser = require("./attributes");
 const expr = require("./expression");
-const strip = require("./strip-elmx");
 const generate = require("./generator");
 const R = require("ramda");
 
-function enableModules(content) {
-  return content.replace(/--(import Html)/g, "$1");
-}
-
-function parse(elmx) {
+module.exports = function(elmx) {
   const state = new State();
-  // let padding = "";
 
   let parser = new htmlparser.Parser({
     onopentag: function(name, attrs) {
       state.enter(name, attrParser(attrs));
     },
     ontext: function(text) {
-      // padding = R.last(text.match(/(?:^|[\r\n])([ \t]*)$/) || [""]);
-
       if (state.isRoot()) {
         state.addCode(text);
         return;
@@ -40,13 +32,5 @@ function parse(elmx) {
   parser.write(elmx);
   parser.end();
 
-  // console.log('ast', state.dump());
-
   return generate(state.get());
 }
-
-module.exports = R.compose(
-  parse,
-  strip,
-  enableModules
-);
