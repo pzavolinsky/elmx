8231fc616b7410c5d03269d082ff02bb65869079
Fredrik Ekholdt <freekh@gmail.com>
2017-04-13 17:11:30 +0200

    Fixed broken examples

--- example/src/Hello.elm
+++ example/src/Hello.elm
@@ -1,9 +1,5 @@
-module Main exposing (..)
-
 import Html
 import Html.Attributes
 
-
 main : Html.Html ()
-main =
-    Html.node "node" "span" [ Html.Attributes.attribute "style" "color: green" ] [ Html.text "Hello, ", Html.node "node" "b" [] [ Html.text "elmx!" ] ]
+main = Html.span [Html.Attributes.attribute "style" "color: green"] [Html.text "Hello, ", Html.b [] [Html.text "elmx!"]]
--- example/src/Page.elm
+++ example/src/Page.elm
@@ -5,16 +5,16 @@ import List exposing (map)
 main : Html.Html ()
 main =
   let
-    hello = Html.node "h1" [] [Html.text "Hello"]
+    hello = Html.h1 [] [Html.text "Hello"]
     name = "Homer"
-    lis = map (\s -> Html.node "li" [] [Html.text s]) [ "Bart", "List", "Maggie" ]
+    lis = map (\s -> Html.li [] [Html.text s]) [ "Bart", "List", "Maggie" ]
     commonAttrs =
       [ title "common title"
       , align "left"
       ]
   in
-    Html.node "div" ([Html.Attributes.attribute "class" "container"] ++ commonAttrs) [
+    Html.div ([Html.Attributes.attribute "class" "container"] ++ commonAttrs) [
       hello
       , Html.text name, Html.text " is the father of:
-      ", Html.node "ul" [] (lis)
+      ", Html.ul [] (lis)
     ]
