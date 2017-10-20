c49c08bb8d9254f0f680a8d4c701b5eed04d5da6
Chase Pursley <chasepursley@gmail.com>
2017-10-20 15:58:33 -0400

    [EXAMPLE] Fix syntax and conform to elm-format.

--- example/src/Page.elm
+++ example/src/Page.elm
@@ -1,20 +1,29 @@
+module Page exposing (..)
+
 import Html
 import Html.Attributes exposing (title, align)
 import List exposing (map)
 
+
 main : Html.Html ()
 main =
-  let
-    hello = Html.h1 [] [Html.text "Hello"]
-    name = "Homer"
-    lis = map (\s -> Html.li [] [Html.text s]) [ "Bart", "List", "Maggie" ]
-    commonAttrs =
-      [ title "common title"
-      , align "left"
-      ]
+    let
+        hello =
+            Html.h1 [] [Html.text "Hello"]
+
+        name =
+            "Homer"
+
+        lis =
+            [ "Bart", "List", "Maggie" ] |> map (\s -> Html.li [] [Html.text s])
+
+        commonAttrs =
+            [ title "common title"
+            , align "left"
+            ]
   in
-    Html.div ([Html.Attributes.attribute "class" "container"] ++ commonAttrs) [
-      hello
-      , Html.text name, Html.text " is the father of:
-      ", Html.ul [] (lis)
-    ]
+      Html.div ([Html.Attributes.attribute "class" "container"] ++ commonAttrs) [
+        hello
+        , Html.p [] [Html.text name, Html.text " is the father of: "]
+        , Html.ul [] (lis)
+      ]
